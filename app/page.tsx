aa4dd5bdaffca3f0df0f5a2fa570144a5f01f510
Takekuni <takevadogu@outlook.jp>
2024-04-24 19:12:14 +0900

    SSEで、BBSのAPIをfetchで取得

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import BBSCardList from "./components/BBSCardList";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/post", {
+    cache: "no-store",
+  });
+
+  const bbsAllData = await response.json();
+  console.log(bbsAllData);
+
   return (
     <main>
       <BBSCardList />
